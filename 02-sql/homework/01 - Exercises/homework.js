1db25455fa2827d6392f32250e75be776d595cee
Lisandro <lisandroviedo@hotmail.com>
2023-12-01 09:45:45 -0400

    M4 01/12/2023

--- 02-sql/homework/01 - Exercises/homework.js
+++ 02-sql/homework/01 - Exercises/homework.js
@@ -1,22 +1,24 @@
 /* ¡Escribe tus comandos en este archivo! */
 
-const ejercicio02 = '';
+const ejercicio02 = "select * from movies where duration < 90;";
 
-const ejercicio03 = '';
+const ejercicio03 = "select * from movies where year between 1930 and 1940;";
 
-const ejercicio04 = '';
+const ejercicio04 = "select * from movies where title like '%til%';";
 
-const ejercicio05 = '';
+const ejercicio05 = "select * from movies where cardinality(actors)=1;";
 
-const ejercicio06 = '';
+const ejercicio06 =
+  "select title, avg(rating) from movies, unnest(ratings) as rating group by title;";
 
-const ejercicio07 = '';
+const ejercicio07 =
+  "select actors from movies where title like '%Fast and%' and year = 2016;";
 
 module.exports = {
-   ejercicio02,
-   ejercicio03,
-   ejercicio04,
-   ejercicio05,
-   ejercicio06,
-   ejercicio07,
+  ejercicio02,
+  ejercicio03,
+  ejercicio04,
+  ejercicio05,
+  ejercicio06,
+  ejercicio07,
 };
