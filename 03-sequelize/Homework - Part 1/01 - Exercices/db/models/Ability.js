0d31174b9b81138f5164291fee73b1d58a792719
Lisandro <lisandroviedo@hotmail.com>
2023-12-04 11:42:47 -0400

    M4 04/12/2023

--- 03-sequelize/Homework - Part 1/01 - Exercices/db/models/Ability.js
+++ 03-sequelize/Homework - Part 1/01 - Exercices/db/models/Ability.js
@@ -1,22 +1,33 @@
 const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
-  sequelize.define("Ability", {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-    },
-    description: {
-      type: DataTypes.TEXT,
-    },
-    mana_cost: {
-      type: DataTypes.FLOAT,
-      allowNull: false,
-      validate: {
-        min: 10.0,
-        max: 250.0,
+  sequelize.define(
+    "Ability",
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.TEXT,
+      },
+      mana_cost: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+        validate: {
+          min: 10.0,
+          max: 250.0,
+        },
       },
     },
-  });
+    {
+      indexes: [
+        {
+          name: "compositeIndex",
+          unique: true,
+          fields: ["name", "mana_cost"],
+        },
+      ],
+    }
+  );
 };
