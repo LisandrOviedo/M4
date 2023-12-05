1904795a24900d28ed6e29464c591fa9330afff1
Lisandro <lisandroviedo@hotmail.com>
2023-12-05 12:35:18 -0400

    M4 05/12/2023

--- 03-sequelize/Homework - Part 1/01 - Exercices/server.js
+++ 03-sequelize/Homework - Part 1/01 - Exercices/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const characterMiddleware = require("./middlewares/character.js");
 // const abilityMiddleware = require("./middlewares/ability.js");
 
+const { Character } = require("./db");
+
 const server = express();
 
 server.use(express.json());
@@ -12,5 +14,28 @@ server.use("/character", characterMiddleware);
 server.get("/", (req, res) => {
   res.send("Henry Sequelize Homework");
 });
+
+// server.post("/character", async (req, res) => {
+//   try {
+//     const { code, name, age, race, hp, mana } = req.body;
+
+//     if (!code || !name || !age || !race || !hp || !mana) {
+//       return res.status(404).send("Falta enviar datos obligatorios");
+//     }
+
+//     const newCharacter = await Character.create({
+//       code,
+//       name,
+//       age,
+//       race,
+//       hp,
+//       mana,
+//     });
+
+//     return res.status(201).json(newCharacter);
+//   } catch (error) {
+//     return res.status(404).send("Error en alguno de los datos provistos");
+//   }
+// });
 
 module.exports = server;
